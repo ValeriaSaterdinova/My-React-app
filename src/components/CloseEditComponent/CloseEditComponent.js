ce5d7862595ac2bae6888d8f6841ac3dbe30fe9f
vsaterdinova <vsaterdinova.exceedteam@gmail.com>
2021-12-14 11:45:47 +0300

    hot fix error

--- src/components/CloseEditComponent/CloseEditComponent.js
+++ src/components/CloseEditComponent/CloseEditComponent.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router';
 import MenuOpenTwoToneIcon from '@material-ui/icons/MenuOpenTwoTone';
 import DeleteSweepTwoToneIcon from '@material-ui/icons/DeleteSweepTwoTone';
 
-
 const CloseEditComponent = ({setCurrent, task, deleteTask}) => {
   const history = useHistory();
-  const {isCheck, text:word} = task;
+  const { isCheck, text:word } = task;
   
   const openEdit = () => {
     setCurrent(task);
